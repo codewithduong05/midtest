2a02be4c437cfeeebfab6d20185ce3ccd8374dca
codewithduong05 <dbui0025@gmail.com>
2026-08-28 20:25:47 +0700

    add docs

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Mindx Test
+
+Test 1 : Thuật toán (Javascript)
+Test 2 : Ứng dụng ( công nghệ : Nextjs, Supabase, Renders )
+Thời gian làm 27/8 - 28/8
--- test2/code-sharing-app/src/lib/db.ts
+++ test2/code-sharing-app/src/lib/db.ts
@@ -10,4 +10,5 @@ export const db =
     log: ['query'],
   })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = db
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
